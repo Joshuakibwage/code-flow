e0e55fa2d1d88502b71deb65dfd25b3556e71f51
Joshua Kibwage <joshuakibwage23@gmail.com>
2026-02-08 13:53:25 +0300

    core: scafold react app with folder structure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrolled(window.scrollY > 50);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar scrolled={scrolled} />
+    </div>
+  );
+}
+
+export default App;
